f7e4988daa4810c007ef3b313361cf1bdc3eeec4
Marko Peric <marko.peric2102@gmail.com>
2024-12-29 20:26:45 +0100

    Added Movie controller, service, and repository with CRUD functions

---
+++ src/main/java/com/example/movies/controllers/MovieControllor.java
@@ -0,0 +1,24 @@
+package com.example.movies.controllers;
+
+import com.example.movies.models.Movie;
+import com.example.movies.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieControllor {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.findAllMovies(), HttpStatus.OK);
+    }
+}
